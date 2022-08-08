eef0f43f57614a04e4526c63b1b2a7263d92b7c0
sergeydomanov <nomad-labs@yandex.ru>
2022-08-08 20:49:31 +0300

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Задание 1");
+        int count1 =0;
+
+        while (count1 < 10) {
+            count1 = count1 + 1;
+            System.out.print(count1 + " "); }
+        System.out.println();
+
+        int count2 =11;
+        for (; count2 > 0;) {
+            count2 = count2 - 1;
+            System.out.print(count2 + " ");
+
+        }
+
+        }
+
     }
-
-
-}
