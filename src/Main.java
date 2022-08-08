f6e24e2f9df8ef321a8e82e5e835ab29cae1ceac
sergeydomanov <nomad-labs@yandex.ru>
2022-08-08 21:29:38 +0300

    added 2nd

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,28 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задание 1");
-        int count1 =0;
+        int count1 = 0;
 
         while (count1 < 10) {
             count1 = count1 + 1;
-            System.out.print(count1 + " "); }
+            System.out.print(count1 + " ");
+        }
         System.out.println();
 
-        int count2 =11;
-        for (; count2 > 0;) {
+        int count2 = 11;
+        for (; count2 > 0; ) {
             count2 = count2 - 1;
             System.out.print(count2 + " ");
 
         }
+        System.out.println();
+        System.out.println("Задание 2");
+
+        int friday = 5;
+        for (; friday <= 31; friday = friday +7) {
+            System.out.println("Сегодня пятница " + friday + " -е число. Необходимо подготовить отчет.");
+
+            }
 
         }
 
